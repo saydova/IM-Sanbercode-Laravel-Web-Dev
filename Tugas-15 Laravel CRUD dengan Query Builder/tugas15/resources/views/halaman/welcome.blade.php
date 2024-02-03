afb456e538af51f55c05e3f4d33db1f3a8d6904f
avodyas@gmail.com <avodyas@gmail.com>
2024-02-03 10:58:55 +0700

    tugas15

---
+++ Tugas-15 Laravel CRUD dengan Query Builder/tugas15/resources/views/halaman/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>selamatdatang, {{$name}}</h1><br>
+       <p>Terimakasih telah bergabung di sanberbook. Social Media kita bersama!</p>
+
+</body>
+</html>
---
+++ Tugas-15 Laravel CRUD dengan Query Builder/tugas15/resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+                <h1>SanberBook</h1>
+                <h3>Social Media Developer Santai Berkualitas</h3>
+                <p>Belajar dan Berbagi agar hidup ini semakin berkualitas</p>
+                <h5>Benefit Join di SanberBook</h5>
+                <ul>
+                    <li>Mendapatkan motivasi dari sesama developer</li>
+                    <li>Sharing knowledge dari para mastah sanber</li>
+                    <li>Dibuat oleh calon developer terbaik</li>
+                </ul>
+                <h5>Cara Bergabung ke SanberBook</h5>
+                <ol>
+                    <li>Mengunjungi Website ini</li>
+                    <li>Mendaftar di <a href="/register" >Form Sign Up</a></li>
+                    <li>Selesai!</li>
+                </ol>
+</body>
+</html>
